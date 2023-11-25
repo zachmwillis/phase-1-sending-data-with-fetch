a8ac049c5cc3ad0dfd0c2ae929781150caaa85e0
Zach Willis <zachmw27@gmail.com>
2023-11-24 19:16:26 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+    const userInfo = {
+        name: name,
+        email: email
+    };
+  
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const userId = data.id;
+        document.body.innerHTML += `<p>${userId}</p>`;
+    })
+    .catch(error => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+}
